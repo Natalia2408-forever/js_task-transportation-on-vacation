bf4e69e5ecc9054aefbaa26be615ef21cc11b868
Natalia Panchuk <Foreverdanda@ukr.net>
2026-01-06 10:49:01 +0200

    solution 2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -8,7 +8,8 @@ function calculateRentalCost(days) {
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
-  const totalSum = days * 40;
+  const COST_PER_DAY = 40;
+  const totalSum = days * COST_PER_DAY;
 
   if (days >= LONG_TERM) {
     return totalSum - LONG_TERM_DISCOUNT;
